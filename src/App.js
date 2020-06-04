db64a9e2bf230b61d0ea45630c2013afdfb8fa20
Alan Eappen <eappenalan123@gmail.com>
2020-06-04 19:41:43 -0400

    Fixing the mutliplte api data display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import ZipCodes from "./ZipCodes"
 import './App.css';
-import ZipCodes from './ZipCodes.js';
 
-class App extends Component {
-  render() {
-    return (
-
-    );
-  }
+class App extends Component{
+ render(){
+   return ( 
+     <div>
+     <div className ="container-fluid bg-dark p-4">
+      </div>
+          <div className ="container">
+          <ZipCodes />
+           </div>  
+           </div>  
+   );
+  
+ }
 }
 
 export default App;
